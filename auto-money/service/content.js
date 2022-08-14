77fd17ac36de7b6d73af78fbb5db5e097307ee3f
Your Name <you@example.com>
2022-08-15 02:06:12 +0900

    Updated

---
+++ auto-money/service/content.js
@@ -0,0 +1,16 @@
+if ( window.location.hostname == 'localhost' ) {
+  var url = 'http://localhost:8080/i/wp-kr/' + 'store/data/auto-money-system/';
+  // console.log( 'Hostname : localhost' );
+} else {
+  var url = 'https://www.wp-kr.com/' + 'store/data/auto-money-system/';
+  // console.log( 'Hostname : XXXXXXXXXX' );
+}
+
+fetch(url)
+.then(res => res.json())
+.then(feed_provider => {
+  let output =
+    '<div>' + feed_provider.content + '</div>';
+  // output += '</tbody></table></div>';
+  document.getElementById('content').innerHTML = output;
+})
