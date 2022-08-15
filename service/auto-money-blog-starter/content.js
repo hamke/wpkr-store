d98b620aa9ad8f375fce019686aea30af8a6c487
Your Name <you@example.com>
2022-08-15 17:46:04 +0900

    Updated

--- service/auto-money-blog-starter/content.js
+++ service/auto-money-blog-starter/content.js
@@ -12,12 +12,23 @@ fetch(url)
   let price_standard = feed_provider.price_standard;
   let price_deluxe = feed_provider.price_deluxe;
   let price_premium = feed_provider.price_premium;
-  let output =
-    '<div>' + feed_provider.content + '</div>';
+
+  let price_standard_list = document.querySelectorAll('.price-standard');
+  let price_deluxe_list = document.querySelectorAll('.price-deluxe');
+  let price_premium_list = document.querySelectorAll('.price-premium');
+
+  for (let i = 0; i < price_standard_list.length; i++) {
+    price_standard_list[i].innerHTML = price_standard;
+  }
+  for (let i = 0; i < price_deluxe_list.length; i++) {
+    price_deluxe_list[i].innerHTML = price_deluxe;
+  }
+  for (let i = 0; i < price_premium_list.length; i++) {
+    price_premium_list[i].innerHTML = price_premium;
+  }
+
+  let output = '<div>' + feed_provider.content + '</div>';
   // output += '</tbody></table></div>';
-  document.querySelector('.price-standard').innerHTML = price_standard;
-  document.querySelector('.price-deluxe').innerHTML = price_deluxe;
-  document.querySelector('.price-premium').innerHTML = price_premium;
   document.getElementById('content').innerHTML = output;
-  console.log(price_deluxe);
+  // console.log(price_deluxe);
 })
