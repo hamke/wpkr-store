5b99b16de3a2898abca2997ac3568fca57dbf3a0
Your Name <you@example.com>
2022-08-16 07:53:09 +0900

    Updated

--- service/auto-seller-premium-articles/content.js
+++ service/auto-seller-premium-articles/content.js
@@ -9,18 +9,22 @@ if ( window.location.hostname == 'localhost' ) {
 fetch(url)
 .then(res => res.json())
 .then(feed_provider => {
-  let price_standard_original = feed_provider.price_standard;
-  let price_standard = price_standard_original;
+  let price_standard_original = feed_provider.price_standard_original;
+  let price_standard = feed_provider.price_standard;
 
-  let price_deluxe_original = feed_provider.price_deluxe;
-  let price_deluxe = price_deluxe_original;
+  let price_deluxe_original = feed_provider.price_deluxe_original;
+  let price_deluxe = feed_provider.price_deluxe;
 
-  let price_premium_original = feed_provider.price_premium;
-  let price_premium = price_premium_original;
+  let price_premium_original = feed_provider.price_premium_original;
+  let price_premium = feed_provider.price_premium;
 
   let price_standard_list = document.querySelectorAll('.price-standard');
   let price_deluxe_list = document.querySelectorAll('.price-deluxe');
   let price_premium_list = document.querySelectorAll('.price-premium');
+
+  let price_standard_original_list = document.querySelectorAll('.price-standard-original');
+  let price_deluxe_original_list = document.querySelectorAll('.price-deluxe-original');
+  let price_premium_original_list = document.querySelectorAll('.price-premium-original');
 
   let summary_standard_list = document.querySelectorAll('.summary-standard');
   let summary_deluxe_list = document.querySelectorAll('.summary-deluxe');
@@ -38,6 +42,15 @@ fetch(url)
   }
   for (let i = 0; i < price_premium_list.length; i++) {
     price_premium_list[i].innerHTML = price_premium.toLocaleString();
+  }
+  for (let i = 0; i < price_standard_original_list.length; i++) {
+    price_standard_original_list[i].innerHTML = price_standard_original.toLocaleString();
+  }
+  for (let i = 0; i < price_deluxe_original_list.length; i++) {
+    price_deluxe_original_list[i].innerHTML = price_deluxe_original.toLocaleString();
+  }
+  for (let i = 0; i < price_premium_original_list.length; i++) {
+    price_premium_original_list[i].innerHTML = price_premium_original.toLocaleString();
   }
 
   for (let i = 0; i < summary_standard_list.length; i++) {
